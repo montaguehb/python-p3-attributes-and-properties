c88adf168c42efb2c300e0e529187d81efdda252
Historia Montague <historia.montague@outlook.com>
2023-05-23 11:13:29 -0600

    Finished Lab

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,33 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name = "dog", breed="Pointer"):
+        self.name = name
+        self.breed = breed
+
+    def get_name(self):
+        return self._name
+    
+    def set_name(self, name):
+        try:
+            if(type(name) == str) and (len(name) >= 1) and (len(name) <= 25):
+                self._name = name 
+            else:
+                raise NameError("Name must be string between 1 and 25 characters.")
+        except NameError as err:
+            print(err)
+
+    def get_breed(self):
+        return self._breed
+    
+    def set_breed(self, breed):
+        try:
+            if(breed in APPROVED_BREEDS):
+                self._breed = breed
+            else:
+                raise LookupError("Breed must be in list of approved breeds.")
+        except LookupError as err:
+            print(err)
+
+    name = property(get_name, set_name)
+    breed = property(get_breed, set_breed)
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,33 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name = "name", job = "Admin"):
+        self.name = name
+        self.job = job
+    
+    def set_name(self):
+        return self._name
+    
+    def get_name(self, name):
+        try:
+            if(type(name) == str) and (len(name) >= 1) and (len(name) <= 25):
+                self._name = name.title() 
+            else:
+                raise NameError("Name must be string between 1 and 25 characters.")
+        except NameError as err:
+            print(err)
+    
+    def set_job(self):
+        return self._job
+    
+    def get_job(self, job):
+        try:
+            if(job in APPROVED_JOBS):
+                self._job = job
+            else:
+                raise LookupError("Job must be in list of approved jobs.")
+        except LookupError as err:
+            print(err)
+    
+    name = property(set_name, get_name)
+    job = property(set_job, get_job)
